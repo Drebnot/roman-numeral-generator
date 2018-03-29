1d07deb75056efbe5c1a37d201d045c6a71ad40d
Aaron Akbari-Mort and Jesse Drebin <aaron@>
2018-03-29 09:15:06 -0700

    add basic HTML & User Interface logic.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// Business Logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+// User Interface Logic
+
+$(document).ready(function() {
+  $("form#form").submit(function(event) {
+    event.preventDefault();
+  });
+});
